d3554d213b3528efb88e976af9b088b5d461c709
BARRETO M <pix.lynlium@gmail.com>
2023-08-11 03:30:34 -0300

    accordion-item click test added

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,6 +1,18 @@
 describe('Fundamentals test', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
+  it('Contains correct header text', () => {
+    cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals-header"]').contains(/Testing Fundamentals/i)
+  })
+
+  it('Accordion works correctly', () => {
+    cy.visit('/fundamentals')
+    cy.get('[data-test="accordion-item-1"]').click()
+    cy.get('[data-test="accordion-item-2"]').click()
+    cy.get('[data-test="accordion-item-3"]').click()
+    cy.get('[data-test="accordion-item-4"]').click()
+    cy.get('[data-test="accordion-item-5"]').click()
+    cy.get('[data-test="accordion-item-6"]').click()
+    cy.get('[data-test="accordion-item-7"]').click()
+    cy.get('[data-test="accordion-item-8"]').click()
   })
 })
